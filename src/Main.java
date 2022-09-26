ce46c266dff47c1a3ffea6c6e947078c8313404f
anna-ermakova <korovanna8@mail.ru>
2022-09-26 21:29:37 +0300

    Задание 4 задача 1-3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+
     //task 1
     System.out.println("Задача 1");
     int dogsCount=0;
@@ -14,5 +15,24 @@ public class Main {
     System.out.println("Значение переменной ratsCount с типом long равно "+ratsCount);
     System.out.println("Значение переменной medicine с типом float равно "+medicine);
     System.out.println("Значение переменной water с типом double равно "+water);
+
+    //task 2
+    double a=2.4;
+    long b=987_678_965_549L;
+    float c=2.786F;
+    boolean e=false;
+    char f=569;
+    short g=-159;
+    int h=27897;
+    byte i=67;
+
+    //task 3
+    System.out.println("Задача 3");
+    byte firstClass=23;//количество учеников Людмилы Павловны
+    byte seсondClass=27;//количество учеников Анны Сергеевны
+    byte thirdClass=30;//количество учеников Екатерины Андреевны
+    short allSheetsCount=480;
+    int allClass=firstClass+seсondClass+thirdClass;
+    System.out.println("На каждого ученика рассчитано "+allSheetsCount/allClass+" листов бумаги");
     }
 }
