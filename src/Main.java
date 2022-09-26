a5b5a3e6b1bd11cee6cf07da4ef033988ac0c003
anna-ermakova <korovanna8@mail.ru>
2022-09-26 20:46:51 +0300

    Задание 4 задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    //task 1
+    System.out.println("Задача 1");
+    int dogsCount=0;
+    byte catsCount=1;
+    short mousesCount=10;
+    long ratsCount=5_000_000L;
+    float medicine=2.2F;
+    double water=1.5;
+    System.out.println("Значение переменной dogsCount с типом int равно "+dogsCount);
+    System.out.println("Значение переменной catsCount типом byte равно "+catsCount);
+    System.out.println("Значение переменной mousesCount с типом short равно "+mousesCount);
+    System.out.println("Значение переменной ratsCount с типом long равно "+ratsCount);
+    System.out.println("Значение переменной medicine с типом float равно "+medicine);
+    System.out.println("Значение переменной water с типом double равно "+water);
     }
 }
